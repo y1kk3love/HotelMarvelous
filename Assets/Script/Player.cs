0c7737be274f962965b871f9786e01d1f0f569fd
김선민 <y1kk3love@gmail.com>
2021-03-24 15:09:34 +0900

    1.4.1
    Character 8 direction move add

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,51 +5,60 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Player : MonoBehaviour
 {
-    private float vertical;
-    private float horizontal;
+    private float velocity = 5f;
+    private float turnspeed = 10f;
 
-    private float verticalraw;
-    private float horizontalraw;
+    private Vector2 input;
+    private float angle;
 
-    private Vector3 targetrotation;
+    private Quaternion targetrotation;
+    private Transform cam;
 
-    private float rotationspeed = 10;
-    private float speed = 200f;
-
-    private Rigidbody rb;
+    private Animator anim;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        anim = gameObject.GetComponent<Animator>();
+        cam = Camera.main.transform;
     }
 
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
+        GetInput();
 
-        horizontalraw = Input.GetAxis("Horizontal");
-        verticalraw = Input.GetAxis("Vertical");
-
-        Vector3 input = new Vector3(horizontal, 0, vertical);
-        Vector3 inputraw = new Vector3(horizontalraw, 0, verticalraw);
-
-        if(input.sqrMagnitude > 1f)
+        if(Mathf.Abs(input.x)< 1 && Mathf.Abs(input.y) < 1)
         {
-            input.Normalize();
-        }
-        if (inputraw.sqrMagnitude > 1f)
-        {
-            inputraw.Normalize();
-        }
-        if (inputraw != Vector3.zero)
-        {
-            targetrotation = Quaternion.LookRotation(input).eulerAngles;
+            anim.SetBool("Run", false);
+            return;
         }
 
-        rb.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(targetrotation.x, Mathf.Round(targetrotation.y / 45) * 45, targetrotation.z), Time.deltaTime * rotationspeed);
+        CalculateDirection();
+        Rotate();
+        Move();
+    }
 
-        Vector3 vel = input * speed * Time.deltaTime;
-        rb.velocity = vel;
+    void GetInput()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void CalculateDirection()
+    {
+        angle = Mathf.Atan2(input.x, input.y);
+        angle = Mathf.Rad2Deg * angle;
+        angle += cam.eulerAngles.y;
+    }
+
+    void Rotate()
+    {
+        targetrotation = Quaternion.Euler(0, angle, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetrotation, turnspeed * Time.deltaTime);
+    }
+
+    void Move()
+    {
+        anim.SetBool("Run", true);
+        transform.position += transform.forward * velocity * Time.deltaTime;
     }
 }
