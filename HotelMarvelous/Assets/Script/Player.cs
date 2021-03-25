0ea61dd0e279838c4330cfbd1fa756d22c52a380
y1kk3love <y1kk3love@gmail.com>
2021-03-25 16:38:13 +0900

    1.4.1
    Player Character 8 Direction Movement add

---
+++ HotelMarvelous/Assets/Script/Player.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour
+{
+    private float velocity = 5f;
+    private float turnspeed = 10f;
+
+    private Vector2 input;
+    private float angle;
+
+    private Quaternion targetrotation;
+    private Transform cam;
+
+    private Animator anim;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        cam = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        GetInput();
+
+        if(Mathf.Abs(input.x)< 1 && Mathf.Abs(input.y) < 1)
+        {
+            anim.SetBool("Run", false);
+            return;
+        }
+
+        CalculateDirection();
+        Rotate();
+        Move();
+    }
+
+    void GetInput()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void CalculateDirection()
+    {
+        angle = Mathf.Atan2(input.x, input.y);
+        angle = Mathf.Rad2Deg * angle;
+        angle += cam.eulerAngles.y;
+    }
+
+    void Rotate()
+    {
+        targetrotation = Quaternion.Euler(0, angle, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetrotation, turnspeed * Time.deltaTime);
+    }
+
+    void Move()
+    {
+        anim.SetBool("Run", true);
+        transform.position += transform.forward * velocity * Time.deltaTime;
+    }
+}
