0ff2408245eed89b37ef3e307a175471607eb1b4
KimSunMin <y1kk3love@gmail.com>
2021-05-11 16:04:58 +0900

    2.0.4
    RandomMap Researching

---
+++ HotelMarvelous/Assets/Script/RandomMap/MapManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ROOMTYPE
+{
+    GUEST,
+    HALLWAY,
+
+}
+
+public class MapManager : MonoBehaviour
+{
+    private int[,] mapboard = new int[0,0];
+
+    private void Start()
+    {
+        
+    }
+}
---
+++ HotelMarvelous/Assets/Script/RandomMap/RoomInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomInfo : MonoBehaviour
+{
+    private string roomtype;
+
+    void Start()
+    {
+        roomtype = gameObject.name;
+
+        if(roomtype.Substring(2,1) == "V")
+        {
+
+        }
+        else if (roomtype.Substring(2, 1) == "H")
+        {
+
+        }
+    }
+}
