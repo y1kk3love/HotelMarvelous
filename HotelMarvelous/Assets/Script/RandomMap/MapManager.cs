2f2aaf82870bf85687633af3d717b0b6fb1580cf
y1kk3love <y1kk3love@gmail.com>
2021-05-19 21:36:16 +0900

    2.0.11
    RandomMap Researching

--- HotelMarvelous/Assets/Script/RandomMap/MapManager.cs
+++ HotelMarvelous/Assets/Script/RandomMap/MapManager.cs
@@ -19,114 +19,25 @@ public enum ROOMDIR
     LEFT
 }
 
-/*
 public class MapManager : MonoBehaviour
 {
-    public GameObject roomprefab;       //임시 나중에 리소스에서 불러오는것 잊지마요!!
-    public GameObject floorprefab;
+    public FloorInfo[,] RoomBoard = new FloorInfo[50, 50];
 
-    public ROOMTYPE roomtype;
+    public GameObject roomPrefab;
+    public GameObject floorPrefab;
 
-    public List<GameObject> instRoomList = new List<GameObject>();
-    public List<Vector3> needRoomPosList = new List<Vector3>();
+    public byte curRoomCount = 0;
 
-    public int[,] mapboard = new int[50, 50];
-    public int curRoomCount = 0;
-    public int maxRoomCount = 5;
-
-    public string XY;
-
-    private void Update()
+    private void Start()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            string[] xyarr = XY.Split(',');
 
-            int _x = int.Parse(xyarr[0]);
-            int _y = int.Parse(xyarr[1]);
-
-
-            Debug.Log(mapboard[_x + 25, _y + 25]);
-        }
-
-        if (Input.GetKeyDown(KeyCode.S))
-            OnclickMapSpawn();
-        if (Input.GetKeyDown(KeyCode.D))
-            OnclickMadelete();
-    }
-
-    public void OnclickMapSpawn()
-    {
-        StartCoroutine(InstnatiateRoom(true));
-    }
-
-    public void OnclickMadelete()
-    {
-        foreach (GameObject go in instRoomList)
-        {
-            Destroy(go);
-        }
-        needRoomPosList.Clear();
-        instRoomList.Clear();
-        mapboard = new int[50, 50];
-        curRoomCount = 0;
-    }
-
-    IEnumerator InstnatiateRoom(bool _start)
-    {
-        if (_start)
-        {
-            GameObject room = Instantiate(roomprefab, new Vector3(0, 0, 0), Quaternion.identity);
-            room.transform.GetComponent<RoomInfo>().roomType = roomtype;
-
-            curRoomCount++;
-            instRoomList.Add(room);
-        }
-        else
-        {
-            for(int i = 0; i < needRoomPosList.Count - 1; i++)
-            {
-                yield return new WaitForSeconds(1f);
-
-                Vector2 v2pos = needRoomPosList[i];
-                byte _type = (byte)needRoomPosList[i].z;
-
-                Debug.Log(v2pos.x + " , " + v2pos.y);
-                if(mapboard[(byte)(v2pos.x + 25), (byte)(v2pos.y + 25)] != (byte)ROOMTYPE.EMPTY)
-                {
-                    continue;
-                }
-                else
-                {
-                    switch (_type)
-                    {
-                        case (byte)ROOMTYPE.HALLWAY:
-                            GameObject guest = Instantiate(roomprefab, v2pos, Quaternion.identity);
-                            guest.transform.GetComponent<RoomInfo>().roomType = ROOMTYPE.HALLWAY;
-
-                            instRoomList.Add(guest);
-                            break;
-                        case (byte)ROOMTYPE.GUEST:
-                            GameObject hallway = Instantiate(roomprefab, v2pos, Quaternion.identity);
-                            hallway.transform.GetComponent<RoomInfo>().roomType = ROOMTYPE.GUEST;
-
-                            instRoomList.Add(hallway);
-                            break;
-                    }
-                }
-            }
-
-            needRoomPosList.Clear();
-        }
-
-        yield return new WaitForSeconds(1f);
-
-        StartCoroutine(InstnatiateRoom(false));
     }
 }
-*/
 
-public class MapManager : MonoBehaviour
+public class FloorInfo
 {
-
+    public byte roomIndex = 0;
+    public Vector2 floorPos = new Vector3(0,0);
+    public GameObject floorObject = null;
+    public ROOMTYPE roomType = ROOMTYPE.EMPTY;
 }
---
+++ HotelMarvelous/Assets/Script/RandomMap/RoomController.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomController : MonoBehaviour
+{
+    public MapManager mapmanager;
+
+    public ROOMTYPE roomType;
+    private ROOMDIR dir;
+
+    private List<Vector2> floorPosArr = new List<Vector2>();
+
+    private GameObject floorPrefab;     //초기화 필요
+
+    private byte centerOnBoard;         //배열의 중심인 (25,25)를 (0,0)으로 놓고 사용하기위한 변수
+    private byte curX, curY;
+
+
+    void Start()
+    {
+        
+        mapmanager = GameObject.Find("MapManager").GetComponent<MapManager>();
+        centerOnBoard = (byte)(mapmanager.RoomBoard.Length / 2);
+
+        floorPrefab = mapmanager.floorPrefab;
+
+        curX = (byte)transform.position.x;
+        curY = (byte)transform.position.y;
+
+        dir = (ROOMDIR)Random.Range(0, System.Enum.GetValues(typeof(ROOMDIR)).Length);
+
+        SpawnRoomFloor();
+    }
+
+    private void SpawnRoomFloor()
+    {
+        byte roomsize = Randomsize();                       //생성해야할 방의 타입을 확인한 후 그에 따라서 랜덤값 반환;
+        byte dirCount = 0;                                  //4가되면 4방향 모두 막혔다는 이야기
+
+        mapmanager.curRoomCount++;                          //타일끼리 비교하기 위한 방의 인덱스값 맵 생성 성공시 ++ (!!!!!!!!!!!!나중에 뒤로 이동시켜서 작성해줘야함!!!!!!!!!!!!!!!)
+
+        for(int i = 0; i < roomsize; i++)
+        {
+            if (i == 0)                                      //첫 타일은 방의 좌표와 같은곳에 생성
+            {
+                GameObject _floor = Instantiate(floorPrefab, new Vector2(curX, curY), Quaternion.identity);
+                _floor.transform.parent = transform;
+
+                SetInfoToBoard(new Vector2(curX, curY), _floor, mapmanager.curRoomCount);
+            }
+            else
+            {
+                if(dirCount > 3)
+                {
+                    if(roomType == ROOMTYPE.HALLWAY)
+                    {
+                        mapmanager.RoomBoard[curX, curY] = null;
+                        Destroy(gameObject);
+                    }
+
+                    roomsize++;
+                    break;
+                }
+
+                switch (dir)
+                {
+                    case ROOMDIR.TOP:
+                        if (mapmanager.RoomBoard[curX, curY + 1] == null)
+                        {
+                            GameObject _floor = Instantiate(floorPrefab, new Vector2(curX, curY + 1), Quaternion.identity);
+                            _floor.transform.parent = transform;
+
+                            SetInfoToBoard(new Vector2(curX, curY + 1), _floor, mapmanager.curRoomCount);
+
+                            curY++;
+                            dir = 0;
+                        }
+                        else
+                        {
+                            dir++;
+                            break;
+                        }
+                        break;
+                    case ROOMDIR.RIGHT:
+                        if (mapmanager.RoomBoard[curX + 1, curY] == null)
+                        {
+                            GameObject _floor = Instantiate(floorPrefab, new Vector2(curX + 1, curY), Quaternion.identity);
+                            _floor.transform.parent = transform;
+
+                            SetInfoToBoard(new Vector2(curX + 1, curY), _floor, mapmanager.curRoomCount);
+
+                            curX++;
+                            dir = 0;
+                        }
+                        else
+                        {
+                            dir++;
+                            break;
+                        }
+                        break;
+                    case ROOMDIR.BOTTOM:
+                        if (mapmanager.RoomBoard[curX, curY - 1] == null)
+                        {
+                            GameObject _floor = Instantiate(floorPrefab, new Vector2(curX, curY - 1), Quaternion.identity);
+                            _floor.transform.parent = transform;
+
+                            SetInfoToBoard(new Vector2(curX, curY - 1), _floor, mapmanager.curRoomCount);
+
+                            curY--;
+                            dir = 0;
+                        }
+                        else
+                        {
+                            dir++;
+                            break;
+                        }
+                        break;
+                    case ROOMDIR.LEFT:
+                        if (mapmanager.RoomBoard[curX - 1, curY] == null)
+                        {
+                            GameObject _floor = Instantiate(floorPrefab, new Vector2(curX - 1, curY), Quaternion.identity);
+                            _floor.transform.parent = transform;
+
+                            SetInfoToBoard(new Vector2(curX - 1, curY), _floor, mapmanager.curRoomCount);
+
+                            curX--;
+                            dir = 0;
+                        }
+                        else
+                        {
+                            dir++;
+                            break;
+                        }
+                        break;
+                }
+            }
+        }
+    }
+
+    private void SpawnFloorWall()
+    {
+        ROOMDIR walldir = 0;
+
+        foreach(Vector2 _pos in floorPosArr)
+        {
+
+        }
+    }
+
+    private void SetInfoToBoard(Vector2 _pos, GameObject _roomobj, byte _index)
+    {
+        FloorInfo floorinfo = new FloorInfo();
+
+        floorinfo.floorPos = _pos;
+        floorinfo.floorObject = _roomobj;
+        floorinfo.roomIndex = _index;
+
+        mapmanager.RoomBoard[PosParse(_pos.x), PosParse(_pos.y)] = floorinfo;
+
+        floorPosArr.Add(_pos);
+    }
+
+    private byte PosParse(float _pos)
+    {
+        //배열에 값을 넣기위해 입력값에 배열 중앙값을 더해서 반환
+        return (byte)(centerOnBoard + _pos);
+    }
+
+    private byte Randomsize()
+    {
+        //방 탑입에 따라 생성해야할 방의 수를 지정
+        byte _value = 0;
+
+        switch (roomType)
+        {
+            case ROOMTYPE.HALLWAY:
+                _value = 2;
+                break;
+            case ROOMTYPE.GUEST:
+                _value = (byte)Random.Range(1, 6);
+                break;
+        }
+
+        return _value;
+    }
+}
