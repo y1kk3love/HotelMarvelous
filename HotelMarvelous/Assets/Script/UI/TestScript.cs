ca0d065660b4015a4e5ecec82d55e7c40bf839f9
김선민 <y1kk3love@gmail.com>
2021-09-30 00:18:55 +0900

    2.4.2
    Player Attack Motion Fix

---
+++ HotelMarvelous/Assets/Script/UI/TestScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public Animator anim;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            anim.SetTrigger("Attack01");
+        }
+    }
+}
