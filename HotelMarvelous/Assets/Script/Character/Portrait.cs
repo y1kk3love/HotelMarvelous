c7954adcb63c73d8885d9805d1374c8c9b8d2223
KimSunMin <y1kk3love@gmail.com>
2021-10-20 13:10:45 +0900

    2.4.16
    Monster Pattern add

---
+++ HotelMarvelous/Assets/Script/Character/Portrait.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portrait : MonoBehaviour
+{
+    public GameObject dart;
+
+    private float timer = 3;
+
+    private bool isAttacking = false;
+
+    void Update()
+    {
+        TargetFoward();
+
+        if (timer >= 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            isAttacking = false;
+        }
+    }
+
+    private void TargetFoward()
+    {
+        Vector3 Monpos = new Vector3(transform.position.x, transform.position.y - 1.5f, transform.position.z);
+
+        Debug.DrawRay(Monpos, transform.forward * 10, Color.red);
+
+        RaycastHit Hit;
+        if (Physics.Raycast(Monpos, transform.forward, out Hit))
+        {
+            if (Hit.transform.CompareTag("Player") && !isAttacking)
+            {
+                timer = 3;
+                isAttacking = true;
+
+                Vector3 startPos = transform.position + new Vector3(0, transform.position.y, 0);
+                Vector3 targetPos = Hit.transform.position + new Vector3(0, 2.5f, 0);
+
+                Vector3 v1 = -(startPos - new Vector3(startPos.x, 0, startPos.z)).normalized;
+                Vector3 v2 = (targetPos - startPos).normalized;
+
+                float rot = Vector3.Dot(v1, v2);
+
+                GameObject _dart = Instantiate(dart, startPos, Quaternion.Euler(0, 0, rot * 90));
+                _dart.transform.GetComponent<Projectile>().targetPos = targetPos - startPos;
+            }
+        }
+    }
+}
---
+++ HotelMarvelous/Assets/Script/Character/Projectile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public Vector3 targetPos;
+
+    private float speed = 1;
+
+    void Update()
+    {
+        transform.position += targetPos * Time.deltaTime * speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.GetComponent<Player>().SetDamage(10);
+
+            Destroy(transform.gameObject);
+        }
+        else
+        {
+            targetPos = new Vector3(0, 0, 0);
+
+            Destroy(transform.gameObject, 3);
+        }
+    }
+}
